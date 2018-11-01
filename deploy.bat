1efc5c245f3b28baa8684b04db54ea8dc29285cd
Nikita Volkov <2545442@gmail.com>
2018-11-02 00:13:42 +0200

    added deploy.bat

---
+++ deploy.bat
@@ -0,0 +1,3 @@
+@rem npm install -g angular-cli-ghpages
+
+call ng build --prod --base-href "https://NikitaVolkov87.github.io/test_task_Melior-Games/" && ngh && echo Done! && timeout 5 && start https://NikitaVolkov87.github.io/test_task_Melior-Games
